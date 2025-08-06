d1c43946e0af23e4d66f7dfd01a0bb1df16e138b
bastian1603 <bastian160305@gmail.com>
2025-08-06 12:46:23 +0700

    add service

--- be/handlers/service.py
+++ be/handlers/service.py
@@ -1,6 +1,20 @@
-def get(func, *args, **kwargs):
+from fastapi import status, HTTPException
+
+def get(func, param):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    default_content = {
+        "status": False,
+        "message": "Internal Server Error"
+    }
+    
     try:
-        return func(*args, **kwargs)
+        result = func(...param)
+        return result
+    
+    except Exception as e:
+        if e:
+            default_content["message"] = e
+        raise HTTPException(status_code=status_code, detail=default_content)
+    
     except:
-
-
+        raise HTTPException(status_code=status_code, detail=default_content)
