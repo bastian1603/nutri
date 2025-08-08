9cba8f4f9e9fecc67c297d42c7e434e06ae6190a
bastian1603 <bastian160305@gmail.com>
2025-08-08 22:51:46 +0700

    menghapus layout dan memingahkannya untuk dipanggil tiap halaman

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -4,23 +4,35 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import { Navbar, NavbarToggle, NavbarLink, NavbarCollapse } from 'flowbite-react'
+
 import WelcomePage from './pages/WelcomePage.jsx';
 import Register from './pages/Register.jsx';
-import Login from './pages/Login.jsx'
+import Login from './pages/Login.jsx';
+import Profile from './pages/Profile.jsx';
+import Layout from './components/Layout.jsx';
+import Home from './pages/Home.jsx'
+
+import 'flowbite'
+// import 'flowbite/dist/flowbite.css'
 
 const router = createBrowserRouter([
   {path: "/", element: <WelcomePage />},
   {path: "/register", element: <Register />},
-  {path: "/login", element: <Login />}
-])
+  {path: "/login", element: <Login />},
+  {path: "/profile", element: <Profile />},
+  {path: "/home", element: <Home />}
+]);
 
 function App() {
 
   return (
     <>
-      <RouterProvider router={router} />
 
+        <RouterProvider router={router} />
+    
     </>
+    
   )
 }
 
