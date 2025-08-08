287b6eae0e3c3b67c1685f8881ffeccf6b7d00ca
bastian1603 <bastian160305@gmail.com>
2025-08-08 22:51:03 +0700

    menambahkan halaman home dan mengubahnya

--- fe/src/pages/Home.jsx
+++ fe/src/pages/Home.jsx
@@ -1,29 +1,66 @@
 import { useEffect, useState } from "react";
-import {DayPicker} from "react-day-picker";
+import { DayPicker } from "react-day-picker";
 import "react-day-picker/style.css";
+
+import InputFood from "../components/InputFood";
+
+
+import Layout from "../components/Layout";
 
 const Home = () => {
     const [ date_picked, set_date_picked ] = useState((new Date().toDateString()));
+    const [ food_data, set_food_data ] = useState({
+       food_name: "asdasdasd",
+       food_calorie: "" 
+    });
+
+    function update_food_data(e){
+        const {name, value} = e.target;
+
+        console.log(name)
+        set_food_data(prev => ({...prev, [name]: value}))
+    }
     
+    useEffect(()=> {
+        // function untuk melihat history makan
+    }, [date_picked]);
+
+
 
     return (
         <>
-            <div>
-                {date_picked}
-            </div>
 
-            <DayPicker 
-            animate
-            mode="single"
-            selected={date_picked}
-            onSelect={(e) => {
-                set_date_picked(e.toDateString());
-            }}
-            footer={
-                date_picked ? `Selected: ${date_picked}` : "Pick a day."
-            }>
+        <Layout>
+                <div>
+                    {date_picked}
+                    <br />
+                    {food_data.food_name}
+                    <br />
+                    {food_data.food_calorie}
+                </div>
 
-            </DayPicker>
+                <DayPicker 
+                    animate
+                    mode="single"
+                    selected={date_picked}
+                    onSelect={(e) => {
+                        set_date_picked(e.toDateString());
+                    }}
+                    footer={
+                        date_picked ? `Selected: ${date_picked}` : "Pick a day."
+                    }>
+                </DayPicker>
+
+                <div className="max-w-md border rounded-sm ring ring-teal-400 p-2 ">   
+                    <h1 className="text-lg pl-2">Add Food</h1>
+
+                    <InputFood className="my-2" id="food_name" name="food_name" placeholder="Sudah makan apa anda hari ini" input_change={update_food_data}/>
+                    <InputFood className="my-2" id="food_calorie" name="food_calorie" placeholder="Berapa Jumlah kalori makanan tersebut?" input_change={update_food_data}/>
+                    
+                    <button className="px-2 rounded-md border border-teal-500 bg-teal-400 text-white" onClick={() => {}}>Add</button>
+
+                </div>
+            </Layout>
         </>
     );
 }
