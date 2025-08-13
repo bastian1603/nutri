c0d1d5a5ad4c63f2c06b40d78364f35b4cdd2ab1
bastian <bastian160305@gmail.com>
2025-08-14 06:52:54 +0700

    migrations baru

--- be/app/migrations/env.py
+++ be/app/migrations/env.py
@@ -7,6 +7,8 @@ from alembic import context
 from app.models import *
 
 from app.db import Base
+
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
